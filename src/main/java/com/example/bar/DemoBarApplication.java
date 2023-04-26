ae1063d780588b9ffa1f16426c4f487b32d5edd7
panuwat <panuwat.codes@gmail.com>
2023-04-26 14:51:47 +0700

    feat: opaque

---
+++ src/main/java/com/example/bar/DemoBarApplication.java
@@ -0,0 +1,13 @@
+package com.example.bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bar/DemoBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
